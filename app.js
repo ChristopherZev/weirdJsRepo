8b7fdc1702e4eff35ea89b8397933e3623c929ea
ChristopherZev <christopher_zev@icloud.com>
2018-05-21 15:04:40 -0400

    Tested Async example

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ console.log(myVar);
 a();
 console.log(myVar);
 /* un,1,2,un,1*/
-
-/* Scope Chain testing
+/*-----------------------------------------------------------*/
+/* Scope Chain testing Outer reference
 
 function a(){
 
@@ -35,3 +35,19 @@ var myVar = 1;
 var myVar2 = 99;
 a();
 */
+/*-----------------------------------------------------------*/
+/*Async Exmaple, Event Queue */
+//long running function
+function wait3sec(){
+  var ms = 3000 + new Date().getTime();
+  while(new Date() < ms){}
+  console.log('finished function');
+}
+function clickHandler() {
+  console.log('click event!');
+}
+//listen for the click event
+document.addEventListener('click',clickHandler);
+
+wait3sec();
+console.log('finished exeution');
