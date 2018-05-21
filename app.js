8eb8b80688b506f801d4614d0325dea27db729f6
ChristopherZev <christopher_zev@icloud.com>
2018-05-21 13:08:30 -0400

    Tested Scope chain

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Variable Enviroments
+
 function b(){
   var myVar;
   console.log(myVar);
@@ -13,3 +15,23 @@ console.log(myVar);
 a();
 console.log(myVar);
 /* un,1,2,un,1*/
+
+/* Scope Chain testing
+
+function a(){
+
+  function b(){
+    console.log(myVar);
+
+    function c(){
+      console.log(myVar2);
+    }
+    c();
+  }
+  b();
+}
+
+var myVar = 1;
+var myVar2 = 99;
+a();
+*/
